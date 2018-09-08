802f03caf9986b636b4694fc5d46b53e457578dd
Michael <michael.glatzmaier@gmail.com>
2018-09-08 10:10:19 -0500

    Start movegen class.

--- move.h
+++ move.h
@@ -1,6 +1,10 @@
 #ifndef MOVE_H
 #define MOVE_H
 
+
+#include "types.h"
+#include "position.h"
+
 //---------------------------------------------
 // Move Encoding:
 // Store move information in a 16-bit word, 
@@ -40,27 +44,57 @@
 // 0011                                check from major (queen/rook)
 //-------------------------------------------------
 
-class movelist {
+class move {
   U16 m;
   int v; 
  public:
-  movelist() {}
-  movelist(const U16& mv, int val = 0) : m(mv), v(val) {}
-  movelist(const movelist& o);
-  movelist(const movelist&& o);
-  movelist& operator=(const movelist& o);
-  movelist& operator=(const movelist&& o);
-  ~movelist() {}
+  move() {}
+  move(const U16& mv, int val = 0) : m(mv), v(val) {}
+  move(const move& o);
+  move(const move&& o);
+  move& operator=(const move& o);
+  move& operator=(const move&& o);
+  ~move() {}
 
-  bool operator()(const movelist& o) const { return o.m == m; }
-  bool operator==(const movelist& o) { return o.m == m; }
+  bool operator()(const move& o) const { return o.m == m; }
+  bool operator==(const move& o) { return o.m == m; }
+};
+//inline bool mvgreater(const move& x, const move& y) { return x.v > y.v; }
+
+
+enum Movetype {
+  promotions = 4,
+  capture_promotions = 8,
+  castle_ks,
+  castle_qs,
+  quiets,
+  captures,
+  evasions,
+  checks,
+  castles,
+  quiet_checks,
+  quiet_nochecks,
+  capture_nochecks,
+  advanced_pawn_pushes,
+  dangerous_checks,
+  all_legal,
+  all_pseudo_legal
 };
 
-inline bool mlgreater(const movelist& x, const movelist& y) { return x.v > y.v; }
-
-
+template<Movetype T, Color c>
 class movegen {
-  
+  int it, last;
+  move list[218]; // max moves in any chess position
+
+  inline void generate(const position& p);
+
+ public:
+  movegen() : it(0), last(0) {}
+  movegen(const position& p) : it(0), last(0) { generate(p); }
+  movegen(const movegen& o) = delete;
+  movegen(const movegen&& o) = delete;
+  movegen& operator=(const movegen& o) = delete;
+  movegen& operator=(const movegen&& o) = delete;
 };
 
 #endif
