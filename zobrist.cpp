a2b5ea4be54dc65c859338db217154ba2452b32d
Michael Glatzmaier <michael.glatzmaier@gmail.com>
2019-10-06 12:54:52 -0500

    Reduce startup time of engine - for pbil regression testing on pi-cluster (!)

--- zobrist.cpp
+++ zobrist.cpp
@@ -1,5 +1,6 @@
 #include "zobrist.h"
 #include "bits.h"
+#include "zobristrands.h"
 
 namespace zobrist {
   U64 piece_rands[Square::squares][2][pieces];
@@ -21,39 +22,46 @@ const unsigned _bits = 25;
 
 bool zobrist::load() {
   
-  const unsigned int N = 1826;
+  const unsigned int N = 1835;
   std::vector<U64> rands;
   util::rand<unsigned int> R;
 
-  bool ok = true;
-  while (rands.size() < N) {
+  //bool ok = true;
+  //unsigned count = 0;
+  //while (rands.size() < N) {
 
-    U64 rand = R.next(); // gen(bits, R);
-    
-    // filter zobrist keys to be unique
-    // and favor smaller values (bits distributed near lower half)
-    if (bits::count(rand) > 4) continue;
-    
-    U64 filter_low = (rand >> 20); // 0x000000FFFFFFFF;
-    if (bits::count(filter_low) >= 3) continue;
+  //  U64 rand = R.next(); // gen(bits, R);
+  //  
+  //  // filter zobrist keys to be unique
+  //  // and favor smaller values (bits distributed near lower half)
+  //  if (bits::count(rand) > 4) continue;
+  //  
+  //  U64 filter_low = (rand >> 20); // 0x000000FFFFFFFF;
+  //  if (bits::count(filter_low) >= 3) continue;
 
-    //U64 filter_high = (rand << 20); // 0xFFFFFFFF;
-    //if (bits::count(filter_high) <= 3) continue;
+  //  //U64 filter_high = (rand << 20); // 0xFFFFFFFF;
+  //  //if (bits::count(filter_high) <= 3) continue;
 
-    for (unsigned i = 0; i < rands.size(); ++i) {
-      ok = (rand != rands[i]);
-      if (!ok) break;
-    }
-    if (ok) rands.emplace_back(rand);
-  }
+  //  for (unsigned i = 0; i < rands.size(); ++i) {
+  //    ok = (rand != rands[i]);
+  //    if (!ok) break;
+  //  }
+  //  
+  //  if (ok) {
+  //    count = ((count + 1) % 6 == 0 ? 0 : count + 1);
+  //    std::string eol((count != 0 ? "), " : "),\n"));
+  //    std::cout << "ULL(" << rand << eol;
+  //    rands.emplace_back(rand);
+  //  }
 
+  //}
 
   // pieces
   unsigned int idx = 0;
   for (Square sq = A1; sq <= H8; ++sq) {
     for (Color c = white; c <= black; ++c) {
       for (Piece p = pawn; p <= king; ++p, ++idx) {
-        piece_rands[sq][c][p] = rands[idx]; // gen(bits, R);//gen.next();
+        piece_rands[sq][c][p] = zobrist_rands[idx]; // gen(bits, R);//gen.next();
       }
     }
   }
@@ -62,26 +70,26 @@ bool zobrist::load() {
   // castle rights
   for (Color c = white; c <= black; ++c) {
     for (int bit = 0; bit < 16; ++bit, ++idx) {
-      castle_rands[c][bit] = rands[idx]; // gen(bits, R);// .next();
+      castle_rands[c][bit] = zobrist_rands[idx]; // gen(bits, R);// .next();
     }
   }
 
   
   // ep
   for (int col = 0; col < 8; ++col, ++idx) {
-    ep_rands[col] = rands[idx]; // gen(bits, R); // gen.next();
+    ep_rands[col] = zobrist_rands[idx]; // gen(bits, R); // gen.next();
   }
 
   // stm
-  stm_rands[white] = rands[idx++]; // gen(bits, R); // .next();
-  stm_rands[black] = rands[idx++]; // gen(bits, R); // .next();
+  stm_rands[white] = zobrist_rands[idx++]; // gen(bits, R); // .next();
+  stm_rands[black] = zobrist_rands[idx++]; // gen(bits, R); // .next();
 
 
   for (int m = 0; m < 512; ++m, idx += 2) {
-    move50_rands[m] = rands[idx]; // gen(bits, R); // .next();
-    hmv_rands[m] = rands[idx + 1]; // gen(bits, R);// .next();
+    move50_rands[m] = zobrist_rands[idx]; // gen(bits, R); // .next();
+    hmv_rands[m] = zobrist_rands[idx + 1]; // gen(bits, R);// .next();
   }
-  
+
   return true;
 }
 
